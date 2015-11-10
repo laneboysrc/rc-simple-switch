772c2e392bbd11078b8b16fbbd7adaa171cbe8bc
Werner Lane <wl-opensource@gmx.net>
2015-11-10 20:38:10 +0800

    Start basic implementation

--- firmware/simple-switch.asm
+++ firmware/simple-switch.asm
@@ -16,43 +16,111 @@
 
     #include    <p10f200.inc>
 
-;    __CONFIG     _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _CLKOUTEN_OFF & _IESO_OFF & _FCMEN_OFF
+    __CONFIG     _WDTE_OFF & _MCLRE_OFF & _CP_OFF
 
+SERVO_IN EQU 0
+OUTPUT_PIN EQU 1
+
+TIMER_RESOLUTION equ 32
+
+TMR_TOO_LOW equ     600 / TIMER_RESOLUTION
+TMR_OFF     equ     1400 / TIMER_RESOLUTION
+TMR_ON      equ     1600 / TIMER_RESOLUTION
 
 ;******************************************************************************
 ;* VARIABLE DEFINITIONS
 ;******************************************************************************
-.data_master UDATA
+.data UDATA
 
-temp    res 1
+pulse_width res 1
 
 
 ;******************************************************************************
 ; Reset vector
 ;******************************************************************************
 .code_reset CODE    0x000
-    goto    Init
+;    goto    Init
 
 
 ;******************************************************************************
 ; Relocatable code section
 ;******************************************************************************
-.code_simple_switch CODE
+.code_main CODE
 
 ;******************************************************************************
 ; Initialization
 ;******************************************************************************
 Init
+    movlw   10000100b   ; Wakeup on pin change disables, weak pull-ups enabled,
+                        ; Timer0 clock source Fosc/4, Prescaler assigned to
+                        ; Timer0, Prescaler 1:32 (= 32us per tick @ 4 MHz)
+    option
 
-;   goto    Main_loop
+    movlw   00000001b   ; GPIO 0 input, all others output
+    tris    6
+
 
 
 ;**********************************************************************
 ; Main program
 ;**********************************************************************
 Main_loop
-    movlw   42
-    movwf   temp
+
+output_is_off
+    ; Wait for servo pulse being low
+    btfsc   GPIO, SERVO_IN
+    goto    $-1
+
+    ; Wait for servo pulse rising edge
+    btfss   GPIO, SERVO_IN
+    goto    $-1
+
+    clrf    TMR0
+
+    ; Wait for servo pulse falling edge
+    ; TODO: check for overflow!
+    btfsc   GPIO, SERVO_IN
+    goto    $-1
+
+    movf    TMR0, W
+    movwf   pulse_width
+    movlw   TMR_TOO_LOW
+    subwf   pulse_width, w
+    bc      output_is_off
+    movlw   TMR_ON
+    subwf   pulse_width, w
+    bnc     output_is_off
+
+    bsf     GPIO, OUTPUT_PIN
+
+output_is_on
+
+    ; Wait for servo pulse being low
+    btfsc   GPIO, SERVO_IN
+    goto    $-1
+
+    ; Wait for servo pulse rising edge
+    btfss   GPIO, SERVO_IN
+    goto    $-1
+
+    clrf    TMR0
+
+    ; Wait for servo pulse falling edge
+    ; TODO: check for overflow!
+    btfsc   GPIO, SERVO_IN
+    goto    $-1
+
+    movf    TMR0, W
+    movwf   pulse_width
+    movlw   TMR_TOO_LOW
+    subwf   pulse_width, w
+    bc      output_is_on
+    movlw   TMR_OFF
+    subwf   pulse_width, w
+    bc      output_is_on
+
+    bcf     GPIO, OUTPUT_PIN
+
     goto    Main_loop
 
 
